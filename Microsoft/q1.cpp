7499716068b5175304fdc2da8a6215189bb2a051
Abhishek-Gurav <79034352+Abhishek-Gurav@users.noreply.github.com>
2023-01-01 10:47:32 +0530

    Add files via upload

---
+++ Microsoft/q1.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(auto x : tokens){
+            if(x == "+"){
+                int k = st.top();
+                st.pop();
+                int l = st.top();
+                st.pop();
+                st.push(k+l);
+                continue;
+            }
+            if(x == "-"){
+                int k = st.top();
+                st.pop();
+                int l = st.top();
+                st.pop();
+                st.push(l-k);
+                continue;
+
+            }
+            if(x == "*"){
+                int k = st.top();
+                st.pop();
+                int l = st.top();
+                st.pop();
+                st.push(k*l);
+                continue;
+
+            }
+            if(x == "/"){
+                int k = st.top();
+                st.pop();
+                int l = st.top();
+                st.pop();
+                st.push(l/k);
+                continue;
+            }
+            st.push(stoi(x));
+        }
+        return st.top();
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    vector<string> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    } 
+    Solution ans;
+    cout << ans.evalRPN(a) << endl;
+}
